47bd68afcfc7f6a2862368755ea41b260cff4366
Rizal Anggoro <gnoogler4@gmail.com>
2023-11-13 19:49:02 +0700

    1948

--- app/dashboard/voter/page.tsx
+++ app/dashboard/voter/page.tsx
@@ -14,6 +14,7 @@ export default async function Page() {
   console.log({ baseUrl });
   const response = await fetch(baseUrl + "api/dashboard/voter", {
     method: "GET",
+    cache: "no-store",
   });
 
   if (!response.ok) {
